22dc6da2c735d49edbcdd599138271ef758dad3e
milosdjurica <milosdjurica13@gmail.com>
2023-11-06 11:57:47 +0100

    added theme switcher

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { ThemeSwitcher } from "@/components/ThemeSwitcher";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-6xl">Weather app with ShadCN</h1>
+      <ThemeSwitcher />
     </div>
   );
 }
