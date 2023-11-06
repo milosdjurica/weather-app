34c88d9537ffe4c031806cffdc3cad0fd8001425
milosdjurica <milosdjurica13@gmail.com>
2023-11-06 13:03:50 +0100

    added axios, created types for response object and called api, all working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { ThemeSwitcher } from "@/components/ThemeSwitcher";
+import getForecast from "@/utils";
 
 export default function Home() {
+  const res = getForecast("Medellin");
+
   return (
     <div>
       <h1 className="text-6xl">Weather app with ShadCN</h1>
       <ThemeSwitcher />
+      <p></p>
     </div>
   );
 }
---
+++ src/utils.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { ResponseData } from "./types";
+
+export default async function getForecast(query: string) {
+  const options = {
+    method: "GET",
+    url: process.env.NEXT_PUBLIC_URL,
+    params: {
+      q: query,
+      days: "3",
+    },
+    headers: {
+      "X-RapidAPI-Key": process.env.NEXT_PUBLIC_API_KEY,
+      "X-RapidAPI-Host": process.env.NEXT_PUBLIC_API_HOST,
+    },
+  };
+  try {
+    const response = await axios.request(options);
+    const data: ResponseData = response.data;
+    console.log(data.forecast.forecastday[0]);
+  } catch (error) {
+    console.error(error);
+  }
+}
